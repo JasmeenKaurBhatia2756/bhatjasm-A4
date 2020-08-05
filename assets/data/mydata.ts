dce87cd9b665a1f5e38e8dc0cdfe09071cee7e87
JasmeenKaurBhatia2756 <68974565+JasmeenKaurBhatia2756@users.noreply.github.com>
2020-08-04 23:33:39 -0400

    Add files via upload

---
+++ assets/data/mydata.ts
@@ -0,0 +1,9 @@
+import { bhatjasm } from '../../app/bhatjasm';
+
+export const MYINFO: bhatjasm = {
+    sid: 991512713,
+    sname: 'Jasmeen Kaur Bhatia',
+    slogin: 'bhatjasm',
+    scampus: 'Davis',
+    stitle: 'Assignment - 4'
+};
